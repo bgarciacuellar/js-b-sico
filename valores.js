6308a310caec283981a2025496b8b8f30f1152f2
Brayan Garcia Cuellar <bgarciacuellar@outlook.com>
2021-04-29 12:00:04 -0500

    Valores, variables, funciones

---
+++ valores.js
@@ -0,0 +1,13 @@
+40
+
+"Brayan Garcia Cuellar"
+
+true 
+false
+
+null
+undefined
+
+[1,2,3]
+
+{ nombre: "Diego"}
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Brayan";
+
+// declarar 
+var edad;
+
+// inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Brayan", 
+    edad: 30
+}
+
+persona;
